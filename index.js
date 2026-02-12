93c9a12ca085f418d29638341b9ecb5300febbb7
muhammadmuzammal266-ops <muhammadmuzammal266@gmail.com>
2026-02-12 23:44:04 +0000

    Refactor index.js to use app.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-const express = require("express");
-const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Trading App – Unlimited Free Price Alerts (Sprint 1)");
-});
-
-app.listen(3000, () => {
-  console.log("Web app running on port 3000");
-});
+// Include the app.js file.
+// This will run the code.
+console.log("entrypoint");
+const app = require("./app/app.js");
